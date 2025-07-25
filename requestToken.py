81a80ee570e3e258a1180dbce369722f0b6a91f5
Alex Rougieri <arougieri2213@proton.me>
2025-07-25 15:53:26 -0700

    fix token secret and add logging for error handling

--- requestToken.py
+++ requestToken.py
@@ -1,10 +1,25 @@
 import requests
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='info.log', encoding='utf-8',
+    level=logging.INFO, format='%(asctime)s %(message)s',
+    datefmt='%m/%d/%Y %H:%M:%S')
 
 def requestAccessToken():
+    try:
+        CLIENT_ID = os.environ["CLIENT_ID"]
+        CLIENT_SECRET = os.environ["CLIENT_SECRET"]
+    except KeyError:
+        CLIENT_ID = "Client ID not available"
+        CLIENT_SECRET = "Client Secret not available"
+        logger.info(f"Token value: {CLIENT_ID}")
+
     url = 'https://accounts.spotify.com/api/token'
     payload = {'grant_type': 'client_credentials',
-        'client_id': '${{ secrets.CLIENT_ID }}',
-        'client_secret': '${{ secrets.CLIENT_SECRET }}'}
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET}
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
     r = requests.post(url, params=payload, headers=headers)
